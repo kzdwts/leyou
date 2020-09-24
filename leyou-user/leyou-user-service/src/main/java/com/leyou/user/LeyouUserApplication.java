5ea1daadb3c89f9f6b0b73b2c12996d511572e91
kangyong <kangyong_wts@sina.com>
2020-09-24 11:59:11 +0800

    初始化用户工程

---
+++ leyou-user/leyou-user-service/src/main/java/com/leyou/user/LeyouUserApplication.java
@@ -0,0 +1,25 @@
+package com.leyou.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import tk.mybatis.spring.annotation.MapperScan;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Description:
+ * @author: kangyong
+ * @date: 2020/9/24 11:49
+ * @version: v1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.leyou.user.mapper")
+public class LeyouUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeyouUserApplication.class, args);
+    }
+
+}
