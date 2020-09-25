f07a93f488d42aca4f9a9c365161f261e29a47c8
kangyong <kangyong_wts@sina.com>
2020-09-25 13:02:13 +0800

    新增短信工程

---
+++ leyou-sms-service/src/main/java/com/leyou/sms/LeyouSmsApplication.java
@@ -0,0 +1,21 @@
+package com.leyou.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Description:
+ * @author: kangyong
+ * @date: 2020/9/25 12:59
+ * @version: v1.0
+ */
+@SpringBootApplication
+public class LeyouSmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeyouSmsApplication.class, args);
+    }
+    
+}
