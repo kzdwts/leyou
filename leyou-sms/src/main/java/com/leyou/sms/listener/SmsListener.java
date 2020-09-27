699f0fcc0f71fa01ad93f402b90481e18fd88dd1
kangyong <kangyong_wts@sina.com>
2020-09-27 13:07:34 +0800

    短信发送功能开发

--- leyou-sms/src/main/java/com/leyou/sms/listener/SmsListener.java
+++ leyou-sms/src/main/java/com/leyou/sms/listener/SmsListener.java
@@ -1,6 +1,18 @@
 package com.leyou.sms.listener;
 
+import com.aliyuncs.exceptions.ClientException;
+import com.leyou.sms.property.SmsProperties;
+import com.leyou.sms.util.SmsUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,4 +24,37 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class SmsListener {
+
+    @Autowired
+    private SmsProperties smsProperties;
+
+    @Autowired
+    private SmsUtils smsUtils;
+
+    /**
+     * 监听短信发送消息
+     *
+     * @param msg
+     */
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(value = "leyou.sms.queue", durable = "true"),
+            exchange = @Exchange(value = "leyou.sms.exchange", ignoreDeclarationExceptions = "true", type = ExchangeTypes.TOPIC),
+            key = {"sms.verify.code"}
+    ))
+    public void listenSms(Map<String, String> msg) throws ClientException {
+        // 非空校验
+        if (null == msg || msg.size() <= 0) {
+            return;
+        }
+
+        // 获取参数
+        String phone = msg.get("phone");
+        String code = msg.get("code");
+       if(StringUtils.isBlank(phone) || StringUtils.isBlank(code)) {
+           return;
+       }
+
+       // 发送短信
+        smsUtils.sendSms(phone, code, smsProperties.getSignName(), smsProperties.getVerifyCodeTemplate());
+    }
 }
