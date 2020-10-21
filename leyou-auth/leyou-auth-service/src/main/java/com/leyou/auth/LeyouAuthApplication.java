07b691e32cc59d15513d71e778100d3a1b1b1aa2
kangyong <kangyong_wts@sina.com>
2020-10-21 22:59:19 +0800

    读取jwt配置

--- leyou-auth/leyou-auth-service/src/main/java/com/leyou/auth/LeyouAuthApplication.java
+++ leyou-auth/leyou-auth-service/src/main/java/com/leyou/auth/LeyouAuthApplication.java
@@ -1,7 +1,9 @@
 package com.leyou.auth;
 
+import com.leyou.auth.config.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -16,6 +18,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableConfigurationProperties(JwtProperties.class)
 public class LeyouAuthApplication {
 
     public static void main(String[] args) {
