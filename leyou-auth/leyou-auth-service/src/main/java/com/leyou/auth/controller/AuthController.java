50515a9add27f397dbc65986194c4492991f5f8b
kangyong <kangyong_wts@sina.com>
2020-11-18 22:42:42 +0800

    新增根据token获取用户信息接口

--- leyou-auth/leyou-auth-service/src/main/java/com/leyou/auth/controller/AuthController.java
+++ leyou-auth/leyou-auth-service/src/main/java/com/leyou/auth/controller/AuthController.java
@@ -1,16 +1,16 @@
 package com.leyou.auth.controller;
 
 import com.leyou.auth.config.JwtProperties;
+import com.leyou.auth.entity.UserInfo;
 import com.leyou.auth.service.AuthService;
+import com.leyou.auth.utils.JwtUtils;
 import com.leyou.common.utils.CookieUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -45,7 +45,8 @@ public class AuthController {
     public ResponseEntity<Void> authentication(@RequestParam("username") String username,
                                                @RequestParam("password") String password,
                                                HttpServletRequest request,
-                                               HttpServletResponse response) {
+                                               HttpServletResponse response
+    ) {
         // 登录校验
         String token = this.authService.accredit(username, password);
         if (StringUtils.isBlank(token)) {
@@ -55,5 +56,37 @@ public class AuthController {
         CookieUtils.setCookie(request, response, jwtProp.getCookieName(), token, jwtProp.getCookieMaxAge(), null, true);
         return ResponseEntity.ok().build();
     }
+
+    /**
+     * 根据token获取用户信息
+     *
+     * @param token
+     * @return
+     */
+    @GetMapping("/verify")
+    public ResponseEntity verify(
+            @CookieValue("LY_TOKEN") String token,
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+        try {
+            // 通过jwt工具类使用公钥解析jwt
+            UserInfo user = JwtUtils.getInfoFromToken(token, this.jwtProp.getPublicKey());
+            if (user == null) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+
+            // 重新生成token
+            token = JwtUtils.generateToken(user, jwtProp.getPrivateKey(), jwtProp.getExpire());
+            // 重新写入cookie
+            CookieUtils.setCookie(request, response, jwtProp.getCookieName(), token, jwtProp.getExpire() * 60);
+
+            return ResponseEntity.ok(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
 
 }
